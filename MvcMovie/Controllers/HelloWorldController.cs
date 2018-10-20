fb7629a2b96f03a33a0466f91d5934d67803337b
Jacob Russell <jacob.russell82@outlook.com>
2018-10-19 23:03:35 -0400

    adds controller and maping for webpage

---
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        //
+        // GET: /HelloWorld/
+
+        public string Index()
+        {
+            return "This is my defaul action...";
+        }
+
+        //
+        // GET: /HellowWorld/Welcome/
+        // Requires using System.Text.Encodings.Web
+        public string Welcome(string name, int ID = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
+        }
+    }
+}
